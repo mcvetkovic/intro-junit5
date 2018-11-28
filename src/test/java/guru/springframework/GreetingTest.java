bedc31d068f64a5566e57621baa8d96edbcd1bc6
Milos Cvetkovic <net.milos.cvetkovic@mhp.com>
2018-11-28 14:05:36 +0100

    junit life

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,12 +1,25 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 class GreetingTest {
 
+    private Greeting greeting;
+
+    @BeforeAll
+    public static void start(){
+        System.out.println("START!");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In Before Each...");
+
+        greeting = new Greeting();
+    }
+
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
 
         System.out.println(greeting.helloWorld());
 
@@ -14,8 +27,24 @@ class GreetingTest {
 
     @Test
     void helloWorld1() {
-        Greeting greeting = new Greeting();
 
         System.out.println(greeting.helloWorld("John"));
     }
+
+    @Test
+    void helloWorld2() {
+
+        System.out.println(greeting.helloWorld("Sam"));
+    }
+
+    @AfterEach
+    void tearDown(){
+        System.out.println("In After Each...");
+    }
+
+    @AfterAll
+    public static void end(){
+        System.out.println("FINISH!");
+    }
+
 }
